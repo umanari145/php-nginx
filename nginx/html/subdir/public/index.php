d09efac109ea1569fe7b1d8b4a9d2c5fb1101245
umanari145 <umanari145@gmail.com>
2021-05-29 11:51:33 +0900

    サブディレクトリでのアクセスに成功

--- nginx/html/subdir/public/index.php
+++ nginx/html/subdir/public/index.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/style.css">
     <title>test</title>
 </head>
 <body>
+    <?php
+        echo '本日は晴天なり';
+    ?>
     test
 </body>
 </html>
